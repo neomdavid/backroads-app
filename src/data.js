bfa0ca033a4671a8aae8f5f5b0991293098f72ea
“neomdavid” <neomanaogdavid@gmail.com>
2024-07-01 16:18:13 +0800

    Initial commit for backroads application

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const pageLinks = [
+  { id: 1, href: "#home", text: "home" },
+  { id: 2, href: "#about", text: "about" },
+  { id: 3, href: "#services", text: "services" },
+  { id: 4, href: "#tours", text: "tours" },
+];
+
+export const socialLinks = [
+  { id: 11, href: "https://www.twitter.com", icon: "fa-twitter" },
+  { id: 12, href: "https://www.facebook.com", icon: "fa-facebook" },
+  { id: 13, href: "https://www.twitter.com", icon: "fa-squarespace" },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
