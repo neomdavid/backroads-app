7ecb43c3061c24ad41b2e28eda7367037a38d9d8
“neomdavid” <neomanaogdavid@gmail.com>
2024-07-02 23:23:45 +0800

    Iterate through data to shorten repetitive code

--- src/data.js
+++ src/data.js
@@ -1,3 +1,7 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+
 export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
@@ -29,5 +33,38 @@ export const services = [
     icon: "fas fa-socks fa-fw",
     title: "amazing comfort",
     text: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    info: "  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "china",
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: "october 1th, 2020",
+    title: "best of java",
+    info: "   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "china",
+    duration: 11,
+    cost: 1400,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: "september 15th, 2020",
+    title: "explore hong kong",
+    info: "   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque  vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "hon kong",
+    duration: 8,
+    cost: 5000,
   },
 ];
